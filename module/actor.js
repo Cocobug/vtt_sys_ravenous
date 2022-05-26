8fccdd9f10f53129cd0d470c39818dc62fd4667f
Maximilien Rigaut <max.rigaut@gmail.com>
2022-05-26 21:04:29 +0200

    Hard Refactor

--- module/actor.js
+++ module/actor.js
@@ -28,7 +28,7 @@ export class SimpleActor extends Actor {
    * @type {boolean}
    */
   get isTemplate() {
-    return !!this.getFlag("worldbuilding", "isTemplate");
+    return !!this.getFlag("vtt_sys_ravenous", "isTemplate");
   }
 
   /* -------------------------------------------- */
@@ -40,7 +40,7 @@ export class SimpleActor extends Actor {
 
     // Copy the actor's system data
     const data = this.toObject(false).data;
-    const shorthand = game.settings.get("worldbuilding", "macroShorthand");
+    const shorthand = game.settings.get("vtt_sys_ravenous", "macroShorthand");
     const formulaAttributes = [];
     const itemAttributes = [];
 
--- module/item-sheet.js
+++ module/item-sheet.js
@@ -10,8 +10,8 @@ export class SimpleItemSheet extends ItemSheet {
   /** @inheritdoc */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["worldbuilding", "sheet", "item"],
-      template: "systems/worldbuilding/templates/item-sheet.html",
+      classes: ["vtt_sys_ravenous", "sheet", "item"],
+      template: "systems/vtt_sys_ravenous/templates/item-sheet.html",
       width: 520,
       height: 480,
       tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
